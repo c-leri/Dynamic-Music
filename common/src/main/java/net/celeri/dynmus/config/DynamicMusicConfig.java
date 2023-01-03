bcc798c1c7b7f710e76d83bc8958211406ad03c6
celeri <celestin.bouchet@gmail.com>
2023-01-03 14:18:04 +0100

    Modified the config screen structure

--- common/src/main/java/net/celeri/dynmus/config/DynamicMusicConfig.java
+++ common/src/main/java/net/celeri/dynmus/config/DynamicMusicConfig.java
@@ -3,17 +3,8 @@ package net.celeri.dynmus.config;
 import eu.midnightdust.lib.config.MidnightConfig;
 
 public class DynamicMusicConfig extends MidnightConfig {
-    @Entry(min = 0) public static int searchRange = 5;
-
-    @Entry(isSlider = true, min = 0, max = 15) public static int darknessCap = 8;
-
-    @Entry(min = 0f, max = 1f) public static double darknessPercent = 0.3;
-
-    @Entry(min = 0f, max = 1f) public static double stonePercent = 0.15;
-
-    @Entry(min = 0) public static int pseudoMineshaftSearchRange = 2;
-
-    @Entry(min = 0f, max = 1f) public static double pseudoMineshaftPercent = 0.1;
+    // Music Types Toggles
+    @Comment public static Comment musicTypesTitle;
 
     @Entry public static boolean caveMusic = true;
 
@@ -29,9 +20,30 @@ public class DynamicMusicConfig extends MidnightConfig {
 
     @Entry public static boolean endBossMusic = true;
 
+    // Dynamic Pitch
+    @Comment public static Comment dynamicPitchTitle;
+
     @Entry public static boolean dynamicPitch = true;
 
     @Entry(min = 0, max = 24000) public static int dynamicPitchAnchor = 18000;
 
     @Entry public static boolean dynamicPitchFaster = false;
+
+    // Cave Detection
+    @Comment public static Comment caveDetectionTitle;
+
+    @Entry(min = 0) public static int searchRange = 5;
+
+    @Entry(isSlider = true, min = 0, max = 15) public static int darknessCap = 8;
+
+    @Entry(min = 0f, max = 1f) public static double darknessPercent = 0.3;
+
+    @Entry(min = 0f, max = 1f) public static double stonePercent = 0.15;
+
+    // Mineshaft Detection
+    @Comment public static Comment mineshaftDetectionTitle;
+
+    @Entry(min = 0) public static int pseudoMineshaftSearchRange = 2;
+
+    @Entry(min = 0f, max = 1f) public static double pseudoMineshaftPercent = 0.1;
 }
